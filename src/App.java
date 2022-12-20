0f6b191c757c41152d893380ba3d39ed91bee6a1
Natália Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2022-12-20 11:33:14 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class MediaIdades {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int contador = 0, media = 0;
+        while(leitor.hasNext()){
+            media+= leitor.nextInt();
+            contador++;
+        }
+        media/=contador;
+        
+        if(media <= 25) { System.out.println("Jovem!"); }
+        else if (media<=60) { System.out.println("Adulta!"); }
+        else { System.out.println("Idosa!"); }
     }
 }
